1a39dbe8f22667bcda22931a500bd83e36de35c6
jlm <jianglingmin@yeah.net>
2022-06-27 20:39:02 +0800

    修改测试路径

--- web_client/src/router/index.js
+++ web_client/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +10,12 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/HomeView.vue')
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/test',
+    name: 'test',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/test.vue')
   },
 
   {
