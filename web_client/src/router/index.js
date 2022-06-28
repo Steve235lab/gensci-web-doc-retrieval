056d4e42d43a6e625c8c1a471e68e36c99030915
penguinskeeper <105185792+penguinskeeper@users.noreply.github.com>
2022-06-28 15:41:24 +0800

    忘记密码

--- web_client/src/router/index.js
+++ web_client/src/router/index.js
@@ -27,6 +27,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/login.vue')
   },
   {
+    path: '/retrieve',
+    name: 'retrieve',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/retrieve.vue')
+  },
+  {
     path: '/register',
     name: 'register',
     // route level code-splitting
