15cd7988a74c21d599a530b1e08fe9fe6470369b
jlm <jianglingmin@yeah.net>
2022-07-01 01:59:39 +0800

    取消注释

--- web_client/src/router/index.js
+++ web_client/src/router/index.js
@@ -33,14 +33,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/login.vue')
   },
-  // {
-  //   path: '/retrieve',
-  //   name: 'retrieve',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/retrieve.vue')
-  // },
+  {
+    path: '/retrieve',
+    name: 'retrieve',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/retrieve.vue')
+  },
   {
     path: '/register',
     name: 'register',
