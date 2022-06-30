da65ba651b68362d434d71c66ff995c1873b74dc
jlm <jianglingmin@yeah.net>
2022-06-30 13:51:08 +0800

    新增视图测试页面

--- web_client/src/router/index.js
+++ web_client/src/router/index.js
@@ -17,7 +17,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/test.vue')
   },
-
+  {
+    path: '/viewtest',
+    name: 'viewtest',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/viewtest.vue')
+  },
   {
     path: '/login',
     name: 'login',
