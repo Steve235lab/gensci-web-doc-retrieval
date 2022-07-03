9cd15053b4c451a2c9c1fb32674a34a109fcba8e
jlm <jianglingmin@yeah.net>
2022-07-03 13:26:20 +0800

    增加加载动画及换行处理

--- web_client/src/components/paper_details.vue
+++ web_client/src/components/paper_details.vue
@@ -1,28 +1,31 @@
 <template>
-  <div>
-  <div v-for="(row,item) in paper_details" :key="row" v-show="row">
-    <p>
-      <el-row :gutter="10">
-        <!--                          左侧标题-->
-        <el-col :span="5">
-          <span class="table-expand-label" v-if="item==='Chinese_Title'" style="float: left">&emsp;中文标题 : </span>
-          <span class="table-expand-label" v-else-if="item==='Chinese_Abstract'" style="float: left">&emsp;中文摘要 : </span>
-          <span class="table-expand-label" v-else style="float: left">&emsp;{{ item }} : </span>
-        </el-col>
-        <!--                          右侧具体内容及格式-->
-        <el-col :span="19" >
-          <b v-if="item==='Title'||item ==='Chinese_Title'" style="float: left;text-align:left;">{{ row }}</b>
-          <i v-else-if="item==='Authors'||item==='First_Author'||item==='Corresponding_Author'"
-             style="font-family: 'Times New Roman',serif;float: left;text-align:left;">{{ row }}</i>
-          <span v-else-if="item==='Abstract'||item==='Publication_Type'" v-html="row" style="float: left;text-align:left;"></span>
-          <span v-else style="float: left;text-align:left;" >{{ row }}</span>
-        </el-col>
-      </el-row>
+  <div v-loading="loading" element-loading-text="拼命加载中" id="body">
+    <div v-for="(row,index) in paper_details" :key="row" v-show="row" >
+      <p>
+        <el-row :gutter="10" type="flex" justify="end" style="flex-wrap: wrap;flex-direction: row">
+          <!--                          左侧标题-->
+          <el-col :sm="24" :md="6" :lg="5">
+            <span class="label" v-if="index==='Chinese_Title'" style="float: left">中文标题 : </span>
+            <span class="label" v-else-if="index==='Chinese_Abstract'" style="float: left">中文摘要 : </span>
+            <span class="label" v-else style="float: left">{{ index }} : </span>
+          </el-col>
+          <!--                          右侧具体内容及格式-->
+          <el-col :sm="24" :md="18" :lg="19" >
+            <b v-if="index==='Title'||index ==='Chinese_Title'" style="float: left;text-align:left;">{{ row }}</b>
+            <i v-else-if="index==='Authors'||index==='First_Author'||index==='Corresponding_Author'"
+               style="font-family: 'Times New Roman',serif;float: left;text-align:left;">{{ row }}</i>
+            <span v-else-if="index==='Abstract'" v-html="row" style="float: left;text-align:left;"></span>
+            <span v-else-if="index==='Publication_Type'||index==='Location'||index==='Organization'">
+              <span v-for="item in Line_Feed(row)" :key=item>{{item}}<br/></span>
+            </span>
+            <span v-else style="float: left;text-align:left;" >{{ row }}</span>
+          </el-col>
+        </el-row>
 
 
-    </p>
+      </p>
 
-  </div>
+    </div>
   </div>
 </template>
 
@@ -36,11 +39,33 @@ export default {
         return []
       }
     },
-
+    loading: {
+      type: Boolean,
+      default: function (){
+        return true
+      }
+    },
+  },
+  computed:{
+    Line_Feed: function (){
+      return function (text){
+        return Array.from(new Set(text.split('\n')))
+      }
+    },
   }
 }
 </script>
 
 <style scoped>
-
+.label {
+  width: 180px;
+  display:-moz-inline-box;
+  display:inline-block;
+  color: #A9A9A9;
+}
+#body{
+  margin-left:3%;
+  margin-right:3%;
+  line-height:1.5;
+}
 </style>
