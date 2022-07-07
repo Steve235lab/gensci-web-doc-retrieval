360046cf7ff88dcbaba2230ebe64a0f44982f197
Steve D. J <84221016+Steve235lab@users.noreply.github.com>
2022-07-07 14:15:28 +0800

    常驻线程自启动

--- web_server/manage.py
+++ web_server/manage.py
@@ -24,9 +24,9 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
 
     threading.Thread(target=enable_undeadthread, daemon=True).start()
+    execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
